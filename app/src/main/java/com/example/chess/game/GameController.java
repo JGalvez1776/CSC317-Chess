090c6e06e6e8de142394c90b7b9444ec0d15280d
JGalvez1776 <64766167+JGalvez1776@users.noreply.github.com>
2022-04-09 22:15:58 -0700

    Commented Controller

--- app/src/main/java/com/example/chess/game/GameController.java
+++ app/src/main/java/com/example/chess/game/GameController.java
@@ -1,3 +1,10 @@
+/**
+ * @author: Jaugee Galvez
+ * @description: Servers as a controller class for the game of chess.
+ *               Abstracts the implementation to instead focus on selecting
+ *               pieces to move and where to.
+ */
+
 package com.example.chess.game;
 
 import com.example.chess.game.components.Board;
@@ -12,25 +19,51 @@ public class GameController {
     // a public static final int PIECE_SELECTED = 1;
     // a public static final int PIECE_MOVED = 2;
 
+    /**
+     * Default constructor that initializes with default game of chess
+     */
     public GameController() {
         this.game = new Board();
     }
 
+    /**
+     * Constructor that uses a specific Board object (Allows puzzles)
+     * @param game Board to be contained and used by the controller
+     */
     public GameController(Board game) {
         this.game = game;
     }
 
 
-
+    /**
+     * Grabs the piece at (x, y) and returns its name.
+     * @param x int x coordinate to grab the piece from
+     * @param y int y coordinate to grab the piece from
+     * @return String which is the name of the piece formatted as PlayerPiece EX: BlackPawn
+     */
     public String getPieceName(int x, int y) {
         Piece piece = game.getPiece(x, y);
         return piece != null ? piece.getPlayer() + piece.getName() : null;
         
     }
 
+    /*
+        NOTE: This method is what we want to call when squares are clicked. At the moment
+        it's not fully implemented.
+     */
+
+    /**
+     * Serves the core game logic. Represents "selecting" a square on the board.
+     * First call of select will select a piece at a square.
+     * Second call of select will move the piece to the selected square (If its a valid move)
+     * If during either call, the position contains no piece or is an invalid move, must reselect
+     * @param x int x coordinate to grab the piece from
+     * @param y int y coordinate to grab the piece from
+     * @return int error code. See class's static constants
+     */
     public int select(int x, int y) {
         /*
-            TODO: 
+            TODO: Ignore this comment it's notes for me
             Options:
                 Nothing/Piece unselected - 0
                 Piece first clicked - 1
