56412bb91ef9cbb85a6d4d005732b4031f240f81
minustree <shinysilverwind@gmail.com>
2022-04-25 19:41:25 -0700

    added getselected function in gamecontroller

--- app/src/main/java/com/example/chess/Chessboard.java
+++ app/src/main/java/com/example/chess/Chessboard.java
@@ -95,7 +95,6 @@ public class Chessboard {
 
                 switch (result) {
                     case GameController.NOTHING_SELECTED:
-                        updateBoard();
                         break;
                     case GameController.PIECE_SELECTED:
                         updateBoard();
