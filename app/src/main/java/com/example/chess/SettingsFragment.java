3d868b39b80f7617d89b540aeb1b699c56eb6697
minustree <shinysilverwind@gmail.com>
2022-05-01 18:01:27 -0700

    documented and optimized SettingsFragment

--- app/src/main/java/com/example/chess/SettingsFragment.java
+++ app/src/main/java/com/example/chess/SettingsFragment.java
@@ -1,5 +1,13 @@
+/*
+ * @author: Min Tran
+ * @author: Jaygee Galvez
+ * @description: This fragment handles the settings screen, which allows the user to adjust their
+ * preferences.
+ */
+
 package com.example.chess;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -7,24 +15,35 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
+import java.util.HashMap;
 
 public class SettingsFragment extends Fragment {
 
     private static final int LAYOUT = R.layout.fragment_settings;
-
-    static AppCompatActivity containerActivity;
+    private AppCompatActivity containerActivity;
     private View inflatedView;
+
+    private SharedPreferences sharedPref; private SharedPreferences.Editor editor;
+
+    private static final HashMap<Integer, Integer> themeMap;
+    static {
+        themeMap = new HashMap<>();
+        themeMap.put(R.style.Theme_Classic, R.id.theme1_selection);
+        themeMap.put(R.id.theme1_selection, R.style.Theme_Classic);
+        themeMap.put(R.style.Theme_Wooden, R.id.theme2_selection);
+        themeMap.put(R.id.theme2_selection, R.style.Theme_Wooden);
+        themeMap.put(R.style.Theme_Olive, R.id.theme3_selection);
+        themeMap.put(R.id.theme3_selection, R.style.Theme_Olive);
+        themeMap.put(R.style.Theme_Night, R.id.theme4_selection);
+        themeMap.put(R.id.theme4_selection, R.style.Theme_Night);
+    }
 
     /**
      * Sets container activity.
@@ -41,83 +60,58 @@ public class SettingsFragment extends Fragment {
      * @param savedInstanceState saved instance state
      * @return Inflated view.
      */
+    @SuppressWarnings("ConstantConditions")
+    @SuppressLint("NonConstantResourceId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // get inflated view and shared prefs
         inflatedView = inflater.inflate(LAYOUT, container, false);
-        SharedPreferences sharedPref = containerActivity.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
+        sharedPref = containerActivity.getPreferences(Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
 
-        // setup theme selection
+        // setup initial theme selection
         RadioGroup radioGroup = (RadioGroup) inflatedView.findViewById(R.id.theme_group);
-        switch (sharedPref.getInt("theme", R.style.Theme_Classic)) {
-            case R.style.Theme_Classic:
-                radioGroup.check(R.id.theme1_selection); break;
-            case R.style.Theme_Wooden:
-                radioGroup.check(R.id.theme2_selection); break;
-            case R.style.Theme_Olive:
-                radioGroup.check(R.id.theme3_selection); break;
-            case R.style.Theme_Night:
-                radioGroup.check(R.id.theme4_selection); break;
-        }
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
-        {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case R.id.theme1_selection:
-                        editor.putInt("theme",R.style.Theme_Classic);
-                        break;
-                    case R.id.theme2_selection:
-                        editor.putInt("theme",R.style.Theme_Wooden);
-                        break;
-                    case R.id.theme3_selection:
-                        editor.putInt("theme",R.style.Theme_Olive);
-                        break;
-                    case R.id.theme4_selection:
-                        editor.putInt("theme",R.style.Theme_Night);
-                        break;
-                }
-                editor.apply();
-                containerActivity.setTheme(sharedPref.getInt("theme", R.style.Theme_Classic));
-                ConstraintLayout cl = containerActivity.findViewById(R.id.container);
-                cl.setBackgroundColor(getThemeColor("colorSecondary"));
-            }
+        radioGroup.check(themeMap.get(sharedPref.getInt("theme",R.style.Theme_Classic)));
+
+        // set listener for dynamic theme change
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            editor.putInt("theme",themeMap.get(checkedId));
+            editor.apply();
+            containerActivity.setTheme(sharedPref.getInt("theme", R.style.Theme_Classic));
+            ConstraintLayout cl = containerActivity.findViewById(R.id.container);
+            cl.setBackgroundColor(getThemeColor("colorSecondary"));
         });
 
-        // setup animation toggle
-        SwitchCompat animSwitch = inflatedView.findViewById(R.id.animation_switch);
-        if (sharedPref.getInt("animate",1) == 1) {
-            animSwitch.setChecked(true);
-        } else animSwitch.setChecked(false);
-        animSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    editor.putInt("animate",1);
-                } else editor.putInt("animate",0);
-                editor.apply();
-            }
-        });
-
-        // setup undo toggle
-        SwitchCompat undoSwitch = inflatedView.findViewById(R.id.undo_switch);
-        if (sharedPref.getInt("undo",1) == 1) {
-            undoSwitch.setChecked(true);
-        } else undoSwitch.setChecked(false);
-        undoSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    editor.putInt("undo",1);
-                } else editor.putInt("undo",0);
-                editor.apply();
-            }
-        });
+        // setup toggles
+        setupToggle(R.id.animation_switch,"animate");
+        setupToggle(R.id.undo_switch,"undo");
 
         return inflatedView;
     }
 
+    /**
+     * Sets up switch toggle.
+     * @param viewId id of switch view
+     * @param pref preference that switch controls
+     */
+    public void setupToggle(int viewId, String pref) {
+        SwitchCompat undoSwitch = inflatedView.findViewById(viewId);
+        undoSwitch.setChecked(sharedPref.getInt(pref, 1) == 1);
+        undoSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                editor.putInt(pref,1);
+            } else editor.putInt(pref,0);
+            editor.apply();
+        });
+    }
+
+    /**
+     * Returns the color id of a color given the color name.
+     * @param name color name
+     * @return Color id.
+     */
     public int getThemeColor(String name){
         TypedValue outValue = new TypedValue();
         int colorAttr = containerActivity.getResources().getIdentifier
@@ -125,7 +119,5 @@ public class SettingsFragment extends Fragment {
         containerActivity.getTheme().resolveAttribute(colorAttr, outValue, true);
         return outValue.data;
     }
-
-    // TODO: listeners for radio/switches
 
 }
